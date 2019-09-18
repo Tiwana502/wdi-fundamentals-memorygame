07a8b817d0bc223f4a4d18960911fac5d2185aa0
Tiwana502 <advance4life@yahoo..com>
2019-09-17 22:20:45 -0400

    created game logic update

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,25 @@
 console.log("Up and running!");
 //create array cards
 var cards = ["queen","queen","king","king"];
-var cardOne = cards[0];
-var cardTwo = cards[3];
 var cardsInPlay = []
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
- if (cardsInPlay[0] === cardsInPlay[1]){
-alert("You found a match!");
-}else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[2]) {
-alert("Sorry, try again.");
- }
+function checkForMatch(){
+   if (cardsInPlay.length === 2) {
+     if (cardsInPlay[0] === cardsInPlay[1]){
+       alert("You found a match!");
+      }
+      else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[2]) {
+        alert("Sorry, try again.");
+       }
+    }
 }
+
+
+function flipCard(cardId) {
+  console.log("user flipped" + " " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
