9d1fb8f30f1efc0808f02fcd6920e21992454107
Tiwana502 <advance4life@yahoo..com>
2019-09-20 12:46:15 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,7 +22,7 @@ var cards = [{
 
 }]
 var cardsInPlay = []
-function checkForMatch(){
+function checkForMatch() {
    if (cardsInPlay.length === 2) {
      if (cardsInPlay[0] === cardsInPlay[1]){
        alert("You found a match!");
@@ -34,15 +34,23 @@ function checkForMatch(){
 }
 
 
-function flipCard(cardId) {
-  console.log("user flipped" + " " + cards[cardId].rank);
-  console.log("user flipped" + " " + cards[cardId].suit);
-  cardsInPlay.push(cards[cardId]);
+ var flipCard = function() {
+ var cardId = this.getAttribute("data-id");
+    this.setAttribute("src", cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
 
 
   checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+for (var i = 0; i<cards.length; i++){
+var cardElement = document.createElement("img");
+    cardElement.setAttribute("src","images/back.png");
+    cardElement.setAttribute("data-id",i);
+    cardElement.addEventListener("click",flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+}
+};
+createBoard();
